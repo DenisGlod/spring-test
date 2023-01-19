7d3c3e6c63c9c620a20800b0ce762d3059e556b6
Глод Денис Константинович <denis.glod@mail.ru>
2023-01-19 16:55:56 +0300

    Initial commit

---
+++ spring-recource-server/src/main/java/com/example/springresourceserver/SpringResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.springresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResourceServerApplication.class, args);
+    }
+
+}
---
+++ spring-recource-server/src/main/java/com/example/springresourceserver/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.example.springresourceserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/messages")
+    public String[] getMessages() {
+        return new String[] {"Message 1", "Message 2", "Message 3"};
+    }
+}
